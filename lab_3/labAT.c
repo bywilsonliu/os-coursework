69a7ce386549e7348592a9c05652fb8c31bc968c
liu626 <liu626@mcmaster.ca>
2025-03-07 22:19:55 -0500

    Part I.1 working (minus macro)

--- lab_3/labAT.c
+++ lab_3/labAT.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+//#define BUFFER_SIZE 10; // 11 for fgets?
+//Macros don't seem to work
+
 int main(){
     printf("Hello world!\n");
 
+    FILE *fptr = fopen("labaddr.txt" , "r");
+    char buff[10];
+
+    while(fgets(buff, 10, fptr) != NULL){
+        printf("Virtual address is %s :", buff);
+    }
+
+    fclose(fptr);
     return 0;
 }
