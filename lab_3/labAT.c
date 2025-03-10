3515f1484a20d37213258ab0ba0894b3acc5da81
liu626 <liu626@mcmaster.ca>
2025-03-10 09:59:51 -0400

    Part I.2 done

--- lab_3/labAT.c
+++ lab_3/labAT.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-//#define BUFFER_SIZE 10; // 11 for fgets?
-//Macros don't seem to work
+#define BUFFER_SIZE 10 // 11 for fgets?
+
+#define OFFSET_BITS 12 // 32-20 (counter-intuitive)
+#define OFFSET_MASK 0x00000FFF
+// (0b?)000000000000111111111111111111111111 or 0x000FFF
+#define PAGES 8
+
+// TEST THIS ALL INCREMENTALLY, ONE LINE AT A TIME - ONLY DOING THIS CUZ YOU HAVE NO WIFI ON PLANE
+
 
 int main(){
     printf("Hello world!\n");
 
     FILE *fptr = fopen("labaddr.txt" , "r");
-    char buff[10];
+    char buff[BUFFER_SIZE];
+    u_int32_t page_table[PAGES] = {6,4,3,7,0,1,2,5};
 
-    while(fgets(buff, 10, fptr) != NULL){
-        printf("Virtual address is %s :", buff);
+    while(fgets(buff, BUFFER_SIZE, fptr) != NULL){
+        // printf("Virtual address is %s :", buff);
+        int buff_1 = atoi(buff);
+        u_int32_t buff_2 = (u_int32_t)buff_1;
+        printf("Logical address is %u: ", buff_2);
+        printf("Page# = %u & ", buff_2 >> 12);
+        printf("Offset = %u. \n", buff_2 & OFFSET_MASK);
+        // printf("Physical address is ")
+        
     }
 
     fclose(fptr);
